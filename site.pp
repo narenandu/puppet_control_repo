7f12a8728b964534bb4c2e9ee7fe6cda1a9586ea
Narendra Kumar Vadapalli <narenandu@users.noreply.github.com>
2023-09-05 14:27:38 -0700

    Update site.pp

--- site.pp
+++ site.pp
@@ -3,5 +3,10 @@ node default {
     ensure => file,
     content => 'This is a readme',
     owner   => 'root',
+  },
+  file { '/root/README_FOR_SURE':
+    ensure => file,
+    content => 'This is a readme by me',
+    owner   => 'root',
   }
 }
