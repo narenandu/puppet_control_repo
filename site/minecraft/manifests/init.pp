3eb8170ce54117fb9d5c7b9a316915278bdc869c
Narendra Kumar Vadapalli <narenandu@gmail.com>
2023-09-07 14:05:10 -0700

    Adding minecraft server related module

---
+++ site/minecraft/manifests/init.pp
@@ -0,0 +1,24 @@
+class minecraft{
+  file {'/opt/minecraft':
+    ensure => directory,
+  }
+  file {'/opt/minecraft/minecraft_server.ja':
+    ensure => file,
+    source => 'https://piston-data.mojang.com/v1/objects/84194a2f286ef7c14ed7ce0090dba59902951553/server.jar',
+  }
+  package {'java':
+    ensure => present,
+  }
+  file{'/opt/minecraft/eula.txt':
+    ensure => file,
+    content => 'eula=true',
+  }
+  file{'/etc/systemd/system/minecraft.service':
+    ensure => file,
+    source => 'puppet:///modules/minecraft/minecraft.service'
+  }
+  service{'minecraft':
+    ensure => running,
+    enable => true,
+  }
+}
