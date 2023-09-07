485bd876e6d7d713c0c30b551a4ff3530e0e7c38
Narendra Kumar Vadapalli <narenandu@gmail.com>
2023-09-07 13:21:27 -0700

    writing fqdn to readme file

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,13 +1,12 @@
 node default {
-    file{'/root/README':
-      ensure => file,
-      owner => 'root',
-      content => 'This is a README file',
-    }
 }
 
 node 'master.puppet.vm' {
   include role::master_server
+  file{'/root/README':
+    ensure => file,
+    content => $fqdn,
+  }
 }
 
 node /^web/ {
